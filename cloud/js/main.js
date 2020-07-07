4f3b30b4b4375827bd27731d7fc45b470e988dad
bersen8106 <bersen8106@gmail.com>
2020-07-07 23:13:17 +0500

    Initial commit

---
+++ cloud/js/main.js
@@ -0,0 +1,9 @@
+var cloud = anime({
+    targets: '#cloud',
+    translateX: 30,
+    translateY: 10,
+    easing: 'easeInOutQuad',
+    duration: 5000,
+    direction: 'alternate',
+    loop: true
+});
