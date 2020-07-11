7f62d400c3b08c8d7a77ba992c9cb00e409e51e8
bersen8106 <bersen8106@gmail.com>
2020-07-11 22:18:34 +0500

    Add clouds and JS code

--- cloud/js/main.js
+++ cloud/js/main.js
@@ -1,9 +1,29 @@
 var cloud = anime({
     targets: '#cloud',
-    translateX: 30,
-    translateY: 10,
+    translateX: 130,
+    translateY: 5,
     easing: 'easeInOutQuad',
-    duration: 5000,
+    duration: 15000,
+    direction: 'alternate',
+    loop: true
+});
+
+var cloud1 = anime({
+    targets: '#cloud1',
+    translateX: 100,
+    translateY: 3,
+    easing: 'easeInOutQuad',
+    duration: 8000,
+    direction: 'alternate',
+    loop: true
+});
+
+var cloud2 = anime({
+    targets: '#cloud2',
+    translateX: 100,
+    translateY: 4,
+    easing: 'easeInOutQuad',
+    duration: 14000,
     direction: 'alternate',
     loop: true
 });
