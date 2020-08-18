80f35aa1faf3fd8b1b2d4b254e140e5ee0e60dad
bersen8106 <bersen8106@gmail.com>
2020-08-18 20:53:44 +0500

    All pages

--- gulp/app/js/main.js
+++ gulp/app/js/main.js
@@ -30,11 +30,66 @@ $(function() {
         focusOnSelect: true,
     });
 
-    $('.holder__slider').slick({
+    $('.holder__slider, .shop__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    });
+
+    $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+        var spinner = $(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function() {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+    });
+
+    // $('.quantity-button').on('click', function() {
+    //     let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    //     $('.summ').html('$' + summ);
+    // });
+    $('.quantity-button').on('click', function() {
+        var parents = $(this).parents('.holder-slider__info');
+        let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+        $('.summ', parents).html('$' + summ);
+    });
+    $('.quantity').each(function() {
+        var parents = $(this).parents('.holder-slider__info');
+        let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+        $('.summ', parents).html('$' + summ);
+    });
+
+    // let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    // $('.summ').html('$' + summ);
+
+    $('.surfboard-box__circle').on('click', function() {
+        $(this).toggleClass('active');
     });
 
 });
